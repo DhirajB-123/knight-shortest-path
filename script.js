d56bbe03f373dfa1cf5ae29234e8849d3d68dc74
DhirajB-123 <dhirajbomma@gmail.com>
2022-08-10 19:28:00 -0700

    Back End Logic
    -implemented generation of a graph to contain relations between squares
    -added event listeners on each square to determine the users start and goal
    -applied a breadth first search to find the shortest path from start to goal

---
+++ script.js
@@ -0,0 +1,149 @@
+function coordinatesToString([x, y]) {
+  let string = "";
+  string += x;
+  string += y;
+  return string;
+}
+
+function stringToCoordinates(string) {
+  let x = parseInt(string[0]);
+  let y = parseFloat(string[1]);
+  let coordinates = [x, y];
+  return coordinates;
+}
+
+let inputs = [];
+
+function handleInputs(e) {
+  console.log(e.path[0].id);
+  if (inputs.length == 1) {
+    inputs.push(e.path[0].id);
+    console.log(findShortestPath(inputs[0], inputs[1]));
+    inputs = [];
+  } else if (inputs.length == 0) {
+    inputs.push(e.path[0].id);
+  }
+}
+
+function clickToReset() {
+  console.log("reset");
+}
+
+function generateSquare([x, y], i) {
+  let grid = document.getElementById("board");
+  let square = document.createElement("div");
+  square.id = coordinatesToString([x, y]);
+  square.classList = "square";
+  if (i % 2 === 0) {
+    square.classList.add("dark");
+  }
+  square.addEventListener("click", handleInputs);
+  grid.appendChild(square);
+}
+
+const graph = (function generateGraph() {
+  let pairs = {};
+  const deltas = [
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [1, -2],
+    [-1, -2],
+    [-2, -1],
+    [-2, 1],
+    [-1, 2],
+  ];
+  function generateMoves([x, y]) {
+    let possibleMoves = [];
+    deltas.forEach((change) => {
+      let xNew = x + change[0];
+      let yNew = y + change[1];
+      if (xNew > 0 && xNew < 9 && yNew > 0 && yNew < 9) {
+        possibleMoves.push([xNew, yNew]);
+      }
+    });
+    return possibleMoves;
+  }
+  let i = 0;
+  for (let x = 1; x < 9; x++, i++) {
+    for (let y = 1; y < 9; y++, i++) {
+      pairs[coordinatesToString([x, y])] = generateMoves([x, y]);
+      generateSquare([x, y], i);
+    }
+  }
+  return pairs;
+})();
+
+const nodeFactory = (element) => {
+  let next = null;
+  return { element, next };
+};
+
+const queueFactory = () => {
+  let head = null;
+  let length = 0;
+  function enqueue(element) {
+    let node = nodeFactory(element);
+    let current;
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+  }
+  function dequeue() {
+    if (this.head === null) {
+      return null;
+    } else if (this.length === 1) {
+      let removed = this.head;
+      this.head = null;
+      this.tail = null;
+      length--;
+      return removed;
+    } else {
+      let removed = this.head;
+      this.head = this.head.next;
+      removed.next = null;
+      length--;
+      return removed;
+    }
+  }
+
+  return { head, length, enqueue, dequeue };
+};
+
+let visitedSquares = {};
+
+function findShortestPath(coordString, goal) {
+  visitedSquares[coordString] = null;
+  let queue = queueFactory();
+  queue.enqueue(coordString);
+  while (queue.length != 0) {
+    let current = queue.dequeue().element;
+    if (current === goal) {
+      let path = current;
+      let previous = visitedSquares[current];
+      while (!(previous === null)) {
+        path = previous + path;
+        previous = visitedSquares[previous];
+      }
+      visitedSquares = {};
+      return path;
+    }
+    graph[current].forEach((e) => {
+      edge = coordinatesToString(e);
+      if (!(edge in visitedSquares)) {
+        visitedSquares[edge] = current;
+        queue.enqueue(edge);
+      }
+    });
+  }
+}
